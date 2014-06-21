90bf69bec90148b3da78a901f404d45d77a01a6a
Gary M. Josack <gary@byoteki.com>
2014-06-20 20:04:23 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# clowncar
+
+### Description
+
+Library for simple client-side load balancing.
+
+### Installation
+
+```bash
+pip install clowncar
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+execfile('clowncar/version.py')
+
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+kwargs = {
+    "name": "clowncar",
+    "version": str(__version__),
+    "packages": ["clowncar"],
+    "description": "Store and retrieve TOTP secrets/tokens.",
+    # PyPi, despite not parsing markdown, will prefer the README.md to the
+    # standard README. Explicitly read it here.
+    "long_description": open("README").read(),
+    "author": "Gary M. Josack",
+    "maintainer": "Gary M. Josack",
+    "author_email": "gary@byoteki.com",
+    "maintainer_email": "gary@byoteki.com",
+    "license": "MIT",
+    "install_requires": required,
+    "url": "https://github.com/gmjosack/clowncar",
+    "download_url": "https://github.com/gmjosack/clowncar/archive/master.tar.gz",
+    "classifiers": [
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+}
+
+setup(**kwargs)
